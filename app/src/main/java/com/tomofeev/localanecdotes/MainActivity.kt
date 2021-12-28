0a27c8530f267c36c58d4d97642a93dbcebe23d9
atimofeev <timtut123@gmail.com>
2021-12-28 13:51:34 +0300

    Граф навигации.

--- app/src/main/java/com/tomofeev/localanecdotes/MainActivity.kt
+++ app/src/main/java/com/tomofeev/localanecdotes/MainActivity.kt
@@ -2,10 +2,18 @@ package com.tomofeev.localanecdotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNavigation, navHostFragment.navController)
     }
 }
---
+++ app/src/main/java/com/tomofeev/localanecdotes/anecdotesContainer/AnecdotesContainerFragment.kt
@@ -0,0 +1,20 @@
+package com.tomofeev.localanecdotes.anecdotesContainer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tomofeev.localanecdotes.R
+
+class AnecdotesContainerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_anecdotes_container, container, false)
+    }
+
+}
