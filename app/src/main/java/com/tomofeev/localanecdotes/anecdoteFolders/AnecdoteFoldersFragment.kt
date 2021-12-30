54545ae3e399bbfea1442af1303373bd208192a9
atimofeev <timtut123@gmail.com>
2021-12-30 09:54:08 +0300

    Nested graph

--- app/src/main/java/com/tomofeev/localanecdotes/anecdoteFolders/AnecdoteFoldersFragment.kt
+++ app/src/main/java/com/tomofeev/localanecdotes/anecdoteFolders/AnecdoteFoldersFragment.kt
@@ -1,4 +1,4 @@
-package com.tomofeev.localanecdotes.anecdotesContainer
+package com.tomofeev.localanecdotes.anecdoteFolders
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.tomofeev.localanecdotes.R
 
-class AnecdotesContainerFragment : Fragment() {
+class AnecdoteFoldersFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anecdotes_container, container, false)
+        return inflater.inflate(R.layout.fragment_anecdote_folders, container, false)
     }
 
 }
